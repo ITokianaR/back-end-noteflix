ff86f1d0eb98e6d87515f8c12af8fe7190c77aa7
ITokianaR <tokina.raman@gmail.com>
2022-10-24 10:43:29 +0300

    latest changes

--- app/Http/Controllers/EtudiantController.php
+++ app/Http/Controllers/EtudiantController.php
@@ -6,7 +6,9 @@ use Illuminate\Http\Request;
 use App\Models\Etudiant;
 use App\Models\Niveau;
 use App\Models\Modules;
+use App\Models\Module;
 use App\Models\Note;
+use Illuminate\Support\Facades\DB;
 
 class EtudiantController extends Controller
 {
@@ -50,18 +52,18 @@ class EtudiantController extends Controller
         return $student;
     }
 
+    //api = api/all
     public function all_etudiant() {
-        //$notes = Etudiant::find(1)->notes->where("module_id", $id);
-        $etudiant = Etudiant::where('niveau_id', 1)->get();
-        //api = api/all
-        $note = Etudiant::join('notes', 'notes.etudiant_id', '=', 'etudiants.id')
-            ->join('modules', 'notes.module_id', '=', 'modules.id')
-            ->distinct('matricule')
-            ->get(['notes.*', 'etudiants.*', 'modules.*'])
-            ->where('etudiant_id', '<', 10)
-            ->groupBy('etudiant_id');
+        $data = [];
+        $module = Module::select('id', 'nom_module', 'code')->get();
         
-        return $etudiant;
+        foreach($module as $module) {
+            $data[] = [
+                'module' => $module
+            ];
+        };
+        return $data;
+        
     }
 
     // api/note_etu
@@ -76,5 +78,16 @@ class EtudiantController extends Controller
         $etudiant = Etudiant::where('id', 1)->get();
 
         return $note;
+    }
+    
+    //function for graph
+    public function get_women_by_year() {
+        $w = DB::table('etudiants')
+            ->select(DB::raw('annee as year, count(id) as UserGain'))
+            ->where('genre', 'F')
+            ->groupBy('annee')
+            ->get();
+
+        return $w;
     }
 }
